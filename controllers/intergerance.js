72c6cad1834454884d7991e45e3cdca9dbc0e261
Nikhil <nikhilsunil90s@gmail.com>
2023-02-23 11:18:50 +0530

    intergerance routes

--- controllers/intergerance.js
+++ controllers/intergerance.js
@@ -77,3 +77,90 @@ exports.addNewPropriety = async (req, res, next) => {
         })
     })
 }
+
+exports.saveIntergeranceForm = async (req, res, next) => {
+    let {
+        formId, // blank string for first time, otherwise ID
+        formLanguage, // Hebrew always
+        formAgent, // agent Id
+        commissionType, 
+        transactionType, // array
+        agencies, // array of objectIds
+        properties, // array of objectIds
+        notes, // not mandatory
+        signature
+    } = req.body;
+
+    let datetoday = new Date();
+    let dd = String(datetoday.getDate()).padStart(2, '0');
+    let mm = String(datetoday.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = datetoday.getFullYear();
+    let formGeneratedOn = dd + "-" + mm + "-" + yyyy;
+
+    // implementing visitor side
+    let result = await IntergeranceForm.findById(formId);
+    if (result) {
+        if (signature != "") {
+            let datetoday = new Date();
+            let dd = String(datetoday.getDate()).padStart(2, '0');
+            let mm = String(datetoday.getMonth() + 1).padStart(2, '0'); //January is 0!
+            let yyyy = datetoday.getFullYear();
+            let formSignedOn = dd + "-" + mm + "-" + yyyy;  
+
+            await IntergeranceForm.findByIdAndUpdate(formId, {
+                commissionType: commissionType, 
+                transactionType: transactionType, // array
+                agencies: agencies, // array of objectIds
+                properties: properties, // array of objectIds
+                notes: notes, // not mandatory
+                signature: signature,
+                formSignedOn: formSignedOn
+            }).then(success => {
+                return res.status(200).json({
+                    status: true,
+                    data: success._id,
+                    message: 'Form Updated Successfully!',
+                })
+            }).catch(err => {
+                console.log(err);
+                return res.status(400).json({
+                    status: false,
+                    message: 'Form Update Failed!'
+                })
+            })
+        } else { // the agency employee side
+            let datetoday = new Date();
+            let dd = String(datetoday.getDate()).padStart(2, '0');
+            let mm = String(datetoday.getMonth() + 1).padStart(2, '0'); //January is 0!
+            let yyyy = datetoday.getFullYear();
+            let formSignedOn = dd + "-" + mm + "-" + yyyy; 
+            const newForm = new IntergeranceForm({
+                formLanguage: 'Hebrew',
+                formAgent: formAgent,
+                commissionType: commissionType, 
+                transactionType: transactionType, // array
+                agencies: agencies, // array of objectIds
+                properties: properties, // array of objectIds
+                notes: notes, // not mandatory
+                signature: signature,
+                formGeneratedOn: formGeneratedOn,
+                formSignedOn: formSignedOn,
+            })
+
+            newForm.save().then(success => {
+                return res.status(200).json({
+                    status: true,
+                    data: success._id,
+                    message: 'Form Saved Successfully!'
+                })
+            }).catch(err => {
+                console.log(err);
+                return res.status(400).json({
+                    status: false,
+                    message: 'Form Save Failed! Please Try Again.'
+                })
+            })
+        }
+    }
+
+}
--- routes/routes/intergerance.js
+++ routes/routes/intergerance.js
@@ -4,6 +4,7 @@ const IntergeranceController = require("../../controllers/intergerance");
 
 router.post('/add-partner-agency', IntergeranceController.addNewAgency);
 router.post('/add-propriety', IntergeranceController.addNewPropriety);
+router.post('/save-intergerance-form', IntergeranceController.saveIntergeranceForm);
 
 module.exports = {
     router: router,
