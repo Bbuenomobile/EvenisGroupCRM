fe10c822b3167c957c22dac845da57eaf885aaaf
Nikhil <nikhilsunil90s@gmail.com>
2023-03-06 14:20:47 +0530

    changes

--- routes/routes/intergerance.js
+++ routes/routes/intergerance.js
@@ -9,6 +9,7 @@ router.post('/add-partner-agency', IntergeranceController.addNewAgency);
 router.post('/add-propriety', IntergeranceController.addNewPropriety);
 router.post('/save-intergerance-form', IntergeranceController.saveIntergeranceForm);
 
+
 module.exports = {
     router: router,
     basePath: '/'
